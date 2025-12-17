395a1c482c5645cd86d9c237f617b980a23c303e
bala-amypo <bala@amypo.in>
2025-12-17 09:17:24 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/impl/StudentServiceImpl.java
@@ -0,0 +1,40 @@
+package com.example.demo.service.impl;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import com.example.demo.entity.Student;
+import com.example.demo.repository.StudentRepo;
+import com.example.demo.service.StudentService;
+
+@Service
+public class StudentServiceImpl implements StudentService {
+
+    private final StudentRepo studentRepo;
+
+    public StudentServiceImpl(StudentRepo studentRepo) {
+        this.studentRepo = studentRepo;
+    }
+
+    @Override
+    public Student insertStudent(Student student) {
+        return studentRepo.save(student);
+    }
+
+    @Override
+    public List<Student> getAllStudents() {
+        return studentRepo.findAll();
+    }
+
+    @Override
+    public Optional<Student> getOneStudent(Long id) {
+        return studentRepo.findById(id);
+    }
+
+    @Override
+    public void deleteStudent(Long id) {
+        studentRepo.deleteById(id);
+    }
+}
